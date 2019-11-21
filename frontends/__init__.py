2a19092d2519ecb2c2426cdb1b8e0b8619ceca7d
Louise <louise@zanier.org>
2019-11-21 19:22:31 +0100

    Added .pylintrc

--- frontends/__init__.py
+++
@@ -1,8 +0,0 @@
-# __init__.py ---
-#
-# Filename: __init__.py
-# Author: Louise <louise>
-# Created: Fri Nov 15 17:58:42 2019 (+0100)
-# Last-Updated: Mon Nov 18 14:48:33 2019 (+0100)
-#           By: Louise <louise>
-#
